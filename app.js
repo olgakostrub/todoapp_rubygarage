8dfc04c9d67cebcb16bb10f0a5de60255e7df66d
Olga Gnatenko <olgagnat13@gmail.com>
2017-08-29 02:56:32 +0300

    added heroku config

---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+var port = 3001;
+app.listen(port, function(){
+    console.log('hello world');
+});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path='/' component={App} />
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
